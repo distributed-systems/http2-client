c62e63335349b8250832b2888f909966b3468409
Michael van der Weg <michael@vanderweg.ch>
2018-09-18 00:03:24 +0200

    feat: add better error messages for json requests

--- src/HTTP2Request.mjs
+++ src/HTTP2Request.mjs
@@ -274,24 +274,33 @@ class HTTP2Request extends HTTP2OutgoingMessage {
 
             // wait for the actual response
             this._stream.on('response', (headers) => {
-                
-                // create our custom response stream
-                const response = new HTTP2Response({
-                    stream: this._stream,
-                    headers: headers
-                });
+                (async () => {
 
-                if (this.expectedStatusCodes.size) {
-                    if (!this.expectedStatusCodes.has(response.status())) {
-                        const statusCodesMessage = this.expectedStatusCodes.size === 1 ? 
-                            `${this.expectedStatusCodes.values().next().value}` : 
-                            `one of ${[...this.expectedStatusCodes.values()].join(', ')}`;
+                    // create our custom response stream
+                    const response = new HTTP2Response({
+                        stream: this._stream,
+                        headers: headers
+                    });
 
-                        return reject(new Error(`The response for the ${this.methodName.toUpperCase()} request to '${this.requestURL}' returned the status ${response.status()}, expected the status to be ${statusCodesMessage}!`));
+
+                    if (this.expectedStatusCodes.size) {
+                        if (!this.expectedStatusCodes.has(response.status())) {
+                            const statusCodesMessage = this.expectedStatusCodes.size === 1 ? 
+                                `${this.expectedStatusCodes.values().next().value}` : 
+                                `one of ${[...this.expectedStatusCodes.values()].join(', ')}`;
+
+                            let additionalData = '';
+                            if (response.hasHeader('content-type') && response.getHeader('content-type').toLowerCase() === 'application/json') {
+                                const data = await response.getData();
+                                additionalData = ` (${JSON.stringify(data)})`;
+                            }
+
+                            throw new Error(`The response for the ${this.methodName.toUpperCase()} request to '${this.requestURL}' returned the status ${response.status()}, expected the status to be ${statusCodesMessage}${additionalData}!`);
+                        }
                     }
-                }
 
-                resolve(response);
+                    return response;
+                })().catch(reject).then(resolve);
             });
 
             // send & end
